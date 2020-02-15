09657caee492fac0b2d48a40de41a7d8dbbc6e21
Andrés Felipe Mejía <andres.mejia@symplifica.com>
2020-02-15 18:34:04 -0500

    Adds tests for the correct modeling of the graph

--- src/main/java/com/mejiandres/app/Solution.java
+++ src/main/java/com/mejiandres/app/Solution.java
@@ -6,18 +6,78 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
+/**
+ * Implementation of a class that models the problem <code>find the celebrity </code>
+ * Structure for the problem model and solution was taken from:
+ * https://www.hackerrank.com/challenges/ctci-bfs-shortest-reach/problem
+ * 
+ *  @author Andrés Felipe Mejía
+ *
+ */
 public class Solution {
+	
+	/**
+	 * Class that models a group of people and the relationships between them in terms of
+	 * a knows b as a Graph implemented as a List of adjacency. Every node a in the graph
+	 * will have a list of people they know
+	 * @author Andrés Felipe Mejía
+	 *
+	 */
     public static class Graph {
+    	
+    	LinkedList<Integer> graph[];
         
-        
+        /**
+         * Construct the graph with the number of people on the group
+         * @param size
+         */
         public Graph(int size) {
-            
+            graph = new LinkedList[size];
+            for(int i=0 ; i<size; i++) {
+            	graph[i] = new LinkedList<Integer>();
+            }
         }
 
+        /**
+         * Adds a relationship between nodes (people) in the form
+         * first knows second
+         * @param first
+         * @param second
+         */
         public void addEdge(int first, int second) {
-            
+            if(first < graph.length && first >= 0) {
+            	graph[first].add(second);
+            }
         }
         
+        /**
+         * Returns the number of nodes (people) in the graph
+         * @return total number of nodes
+         */
+        public int numVertex() {
+        	return graph == null ? 0 : graph.length;
+        }
+        
+        /**
+         * Returns the total number of relationships on the group
+         * @return total number of edges
+         */
+        public int numEdges() {
+        	if(graph == null) return 0;
+        	int edges = 0;
+        	for(LinkedList<Integer> list: graph) {
+        		edges += list.size();
+        	}
+        	return edges;
+        }
+        
+        /**
+         * Finds the unique index of the node that fulfills the condition u is known by
+         * everybody, meaning u is in everyone else's list, and u's list is empty
+         * 
+         * @return the index of the node that complies the requirement of celebrity
+         */
         public int findCelebrity() { 
             return -1;
         }
@@ -34,8 +94,8 @@ public class Solution {
     	// a relationship between those two people in the form u knows v
     	// After reading the input the program will ask the Graph class to find the celebrity c
     	// among the group which complies with the condition (everybody knows c and c knows nobody)
-    	// and then the program shall print it, if no node (person) fulfills the requirement -1
-    	// should be printed
+    	// and then the program shall print it, if no node (person) or more than one fulfills the 
+    	// requirement -1 should be printed
         Scanner scanner = new Scanner(System.in);
       
         int queries = scanner.nextInt();
--- src/test/java/com/mejiandres/app/GraphTest.java
+++ src/test/java/com/mejiandres/app/GraphTest.java
@@ -1,14 +1,52 @@
 package com.mejiandres.app;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import com.mejiandres.app.Solution.Graph;
 
 public class GraphTest {
 	
 	@Test
-	public void shouldReturnTrue() {
-		assertTrue(true);
+	public void shouldReturnAnEmptyGraph() {
+		Graph g = new Graph(0);
+		
+		assertEquals(0, g.numVertex());
+	}
+	
+	@Test
+	public void shouldReturnNonEmptyGraph() {
+		Graph g = new Graph(4);
+		
+		assertEquals(4, g.numVertex());
+	}
+	
+	@Test
+	public void shouldReturnZeroEdges() {
+		Graph g = new Graph(3);
+		
+		assertEquals(0, g.numEdges());
+	}
+	
+	@Test
+	public void shouldReturnNonZeroEdges() {
+		Graph g = new Graph(3);
+		
+		g.addEdge(1, 2);
+		g.addEdge(0, 2);
+		
+		assertEquals(2, g.numEdges());
+	}
+	
+	@Test
+	public void shouldIgnoreNonExistentNodes() {
+		Graph g = new Graph(3);
+		
+		g.addEdge(-1, 0);
+		g.addEdge(3, 2);
+		
+		assertEquals(0, g.numEdges());
 	}
 
 }
